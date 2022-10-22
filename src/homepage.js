ae1005deeda3b2bd1c7cc151b2a02087cc1f9767
Andrea Iriarte <99366021+andrea-iriarte@users.noreply.github.com>
2022-10-22 13:10:35 -0400

    Add files via upload

---
+++ src/homepage.js
@@ -0,0 +1,17 @@
+/*const text = document.getElementById("text-box");
+text.onclick = function ()
+{
+
+}*/
+
+
+ const search = document.getElementById("search-button");
+ const input = document.getElementById("search");
+ search.onclick = function(){
+    window.location.href = "pantries.html";
+ };
+
+
+ 
+
+ 
